86cdfe6979ec38bb9529410037b9d87a45162f0e
Faizan Hasan <faizibaba96@gmail.com>
2023-02-27 02:04:35 +0500

    percentage with grade

---
+++ app.js
@@ -0,0 +1,32 @@
+let percentage = prompt("Enter Your Percentage Here");
+
+if (percentage>=79) {
+
+    alert ("You Grade Is A+")
+    
+} 
+
+else if (percentage>=69) {
+    alert("You Grade Is A")
+
+}
+
+else if (percentage>=59) {
+alert("You Grade Is B")
+}
+
+else if (percentage>=49) {
+    alert("You Grade Is C")
+    }
+    
+    else if (percentage>=39) {
+        alert("Your Grade Is D")
+    }  
+    
+    else if (percentage>=33) {
+        alert("You Grade Is E")
+    }
+ 
+    else if (percentage>=0) {
+        alert("You Are Failed")
+        }
